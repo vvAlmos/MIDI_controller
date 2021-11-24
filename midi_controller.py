005ae1766935600cd37dbe9d55c85d0b29e5630d
Álmos Veres-Vitályos <almos.veres-vitalyos@digilent.ro>
2021-11-24 12:56:45 +0200

    creating main script sctructure

---
+++ midi_controller.py
@@ -0,0 +1,93 @@
+import WF_SDK as wf     # import WF instruments
+import MIDI as midi     # import MIDI commands and messages
+import FPGA as fpga     # import FPGA commands
+
+"""-----------------------------------------------------------------------"""
+
+# define connections
+# DIO
+LED_B = 15
+LED_G = 14
+LED_R = 13
+MUX_ADDR_1 = 12
+MUX_ADDR_0 = 11
+MUX_10 = 10     # empty, empty, attack/release time, empty
+MUX_9 = 9       # G#, A, A#, B
+MUX_8 = 8       # E, F, F#, G
+MUX_7 = 7       # C, C#, D, D#
+MUX_6 = 6       # channel 4, modulation 4, timing 4, octave 4 (MSB)
+MUX_5 = 5       # channel 3, modulation 3, timing 3, octave 3
+MUX_4 = 4       # channel 2, modulation 2, timing 2, octave 2
+MUX_3 = 3       # channel 1, modulation 1, timing 1, octave 1 (LSB)
+MUX_2 = 2       # volume, filter: low, effect 3, effect 6
+MUX_1 = 1       # record, filter: middle, effect 2, effect 5
+MUX_0 = 0       # play, filter: high, effect 1, effect 4
+
+# scope
+DRUM_15 = 1
+DRUM_26 = 2
+DRUM_37 = 3
+DRUM_48 = 4
+
+# wavegen
+PWM_REF_CH = 1
+NEG_REF_CH = 2
+
+"""-----------------------------------------------------------------------"""
+
+# connect to the device
+device_handle, device_name = wf.device.open()
+
+# check for connection errors
+wf.device.check_error(device_handle)
+
+# open raveloxmidi
+midi.open()
+
+# configure the FPGA
+fpga.configure(device_name="ArtyS7")
+
+# initialize the static I/O
+# set output pins
+wf.static.set_mode(device_handle, MUX_ADDR_0, output=True)
+wf.static.set_mode(device_handle, MUX_ADDR_1, output=True)
+# set all pins to LOW
+wf.static.set_state(device_handle, MUX_ADDR_0, False)
+wf.static.set_state(device_handle, MUX_ADDR_1, False)
+
+# initialize the oscilloscope
+wf.scope.open(device_handle)
+
+# initialize the logic analyzer
+wf.logic.open(device_handle)
+
+# turn on the power supply
+wf.supplies.switch(device_handle, device_name, True, True, False, 3.3, 0)
+
+# start generating the reference signals
+# PWM reference
+wf.wavegen.generate(device_handle, PWM_REF_CH, wf.wavegen.function.ramp_up, offset=1.65, amplitude=1.65, frequency=100e03, symmetry=100)
+# -5V reference
+wf.wavegen.generate(device_handle, NEG_REF_CH, wf.wavegen.function.dc, -5)
+
+"""-----------------------------------"""
+
+
+
+
+"""-----------------------------------"""
+
+# reset WF instruments
+wf.wavegen.close(device_handle)
+wf.pattern.close(device_handle)
+wf.logic.close(device_handle)
+wf.scope.close(device_handle)
+wf.static.close(device_handle)
+wf.supplies.switch(device_handle, device_name, False, False, False, 0, 0)
+wf.supplies.close(device_handle)
+
+# close raveloxmidi
+midi.close()
+
+# close the connection to the ADP
+wf.device.close(device_handle)
